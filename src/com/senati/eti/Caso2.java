16a059c5b40c04fab7fcbcc827f6217e2a29e6d8
Valencia <Valencia@DESKTOP-V2PV28T>
2021-03-28 18:04:09 -0500

    Proyecto parcial Guía3 [1-10] publicado el 28/03/2021

---
+++ src/com/senati/eti/Caso2.java
@@ -0,0 +1,26 @@
+package com.senati.eti;
+
+import java.util.Scanner;
+
+public class Caso2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Ingrese número....: ");
+		int n = sc.nextInt();
+		
+		int sumatoria = 0; // Acumulador
+		int i = 1;
+		
+		while (i < n) {
+			sumatoria += i;
+			System.out.print(i + " + ");
+			i++;
+			}
+		  
+		System.out.println("= " + sumatoria);
+
+	}
+
+}
